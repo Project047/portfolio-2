c95dc8d8e0a1162d86b6e024caa69187750cdf91
Rushi047 <2020056791@dkasc.ac.in>
2024-08-04 09:35:35 +0530

    new update

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portfolio</title>
+    <!-- comment shotcut -ctrl +k +c -->
+    <!-- font awesome cdn link -->
+    <link rel="Stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0
+ /css/all.min.css" <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <!-- header section starts -->
+
+    <header class="header">
+        <a href="#home" class="logo">Portfolio.</a>
+
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#about">about</a>
+            <a href="#services">services</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#home">home</a>
+
+        </nav>
+
+    </header>
+
+    <!-- headr section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
