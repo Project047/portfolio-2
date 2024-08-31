ed60ef3f02cc3126eeb7a2aebc9a04cc5b563a79
Rushi047 <2020056791@dkasc.ac.in>
2024-08-31 22:54:46 +0530

    new update

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+$conn = mysqli_connect('localhost','root','','contact_db') or die('connection failed');
+if(isset($_POST['send'])){
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $number = mysqli_real_escape_string($conn, $_POST['number']);
+    $msg = mysqli_real_escape_string($conn, $_POST['message']);
+
+    $select_message = mysqli_query($conn,"SELECT * FROM `contact_form` WHERE name = '$name'
+    AND email='$email' AND number ='$number' AND message='$msg'") or die('query failed');
+    
+    if(mysqli_num_rows($select_message)>0){
+        $message[] = 'message sent already';
+
+    }else{
+        mysqli_query($conn,"INSERT INTO `contact_form`(name, email,number,message)
+         VALUES('$name','$email','$number','$msg') ") or die('query failed');
+        $message[] = 'message sent succesfully..!';
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +29,10 @@
     <!-- comment shotcut -ctrl +k +c -->
     <!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+   
+    <!-- aos css link -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+   
     <!-- my custom link -->
     <link rel=" stylesheet" href="css/style.css">
 
@@ -16,6 +41,17 @@
 
 <body>
 
+<?php
+if (isset($message)) {
+    foreach ($message as $message) {
+        echo '
+        <div class="message" data-aos="zoom-out">
+        <span>' . $message . '</span>
+        <i class="fas fa-times" onclick="this.parentElement.remove();"></i></div>';
+    }
+}
+
+?>
     <!-- header section starts -->
 
     <header class="header">
@@ -44,72 +80,72 @@
 
     <!-- home section starts -->
     <section class="home" id="home">
-        <div class="image">
+        <div class="image" data-aos="fade-right">
             <img src="img/user-img.jpg" alt="user image">
         </div>
         <div class="content">
-            <h3> hi , i am rushikesh kore </h3>
-            <span> web designer & developer </span>
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime debitis cupiditate libero recusandae animi quis, in labore? Quos reprehenderit,
+            <h3 data-aos="fade-up" > hi , i am rushikesh kore </h3>
+            <span data-aos="fade-up" > web designer & developer </span>
+            <p data-aos="fade-up" > Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime debitis cupiditate libero recusandae animi quis, in labore? Quos reprehenderit,
                 reiciendis a suscipit beatae aperiam amet, eveniet quaerat vel nulla maiores?</p>
-            <a href="#about" class="btn">about me</a>
+            <a data-aos="fade-up" href="#about" class="btn">about me</a>
         </div>
     </section>
     <!-- home section end -->
 
     <!-- about section starts -->
     <section class="about" id="about">
-        <h1 class="heading"><span>biography</span></h1>
-
-        <div class="biography">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, earum beatae?
+        <h1 class="heading" data-aos="fade-up"><span>biography</span></h1>
+       
+        <div class="biography" >
+            <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, earum beatae?
                 Earum magnam similique id quasi soluta deserunt corporis nisi.</p>
 
             <div class="bio">
-                <h3><span>name :</span>Rushikesh Kore</h3>
-                <h3><span>email :</span>mysteriousman0947@gmail.com</h3>
-                <h3><span>address :</span>kolhapur ,india</h3>
-                <h3><span>phone :</span>+123-456-7890</h3>
-                <h3><span>age :</span>23 years</h3>
-                <h3><span>experience :</span>2+ years</h3>
+                <h3 data-aos="zoom-in"><span>name :</span>Rushikesh Kore</h3>
+                <h3 data-aos="zoom-in"><span>email :</span>mysteriousman0947@gmail.com</h3>
+                <h3 data-aos="zoom-in"><span>address :</span>kolhapur ,india</h3>
+                <h3 data-aos="zoom-in"><span>phone :</span>+123-456-7890</h3>
+                <h3 data-aos="zoom-in"><span>age :</span>23 years</h3>
+                <h3 data-aos="zoom-in"><span>experience :</span>2+ years</h3>
             </div>
-            <a href="#" class="btn">Download CV</a>
+            <a href="#" class="btn"  data-aos="fade-up">Download CV</a>
         </div>
-        <div class="skills">
+        <div class="skills"  data-aos="fade-up">
             <h1 class="heading"><span>skills</span></h1>
             <div class="progress">
-                <div class="bar">
+                <div class="bar" data-aos="fade-left">
                     <h3><span>HTML</span><span>90%</span></h3>
                 </div>
-                <div class="bar">
+                <div class="bar" data-aos="fade-right">
                     <h3><span>CSS</span><span>70%</span></h3>
                 </div>
-                <div class="bar">
+                <div class="bar" data-aos="fade-left">
                     <h3><span>JavaScript</span><span>55%</span></h3>
                 </div>
-                <div class="bar">
+                <div class="bar" data-aos="fade-right">
                     <h3><span>PHP</span><span>55%</span></h3>
                 </div>
             </div>
         </div>
         <div class="edu-exp">
-            <h1 class="heading"><span>education & experience</span></h1>
+            <h1 class="heading"  data-aos="fade-up"><span>education & experience</span></h1>
             <div class="row">
                 <div class="box-container">
-                    <h3 class="title">education</h3>
-                    <div class="box">
+                    <h3 class="title" data-aos="fade-right">education</h3>
+                    <div class="box" data-aos="fade-right">
                         <span>(2020-2021)</span>
                         <h3>web designer</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                             Nisi necessitatibus pariatur minima voluptatem, rem similique?</p>
                     </div>
-                    <div class="box">
+                    <div class="box" data-aos="fade-right">
                         <span>(2021-2022)</span>
                         <h3>web developer</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                             Nisi necessitatibus pariatur minima voluptatem, rem similique?</p>
                     </div>
-                    <div class="box">
+                    <div class="box" data-aos="fade-right">
                         <span>(2022-2023)</span>
                         <h3>Android developer</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -118,21 +154,21 @@
 
                 </div>
 
-                <div class="box-container">
-                    <h3 class="title">experience</h3>
-                    <div class="box">
+                <div class="box-container" >
+                    <h3 class="title"data-aos="fade-left">experience</h3>
+                    <div class="box" data-aos="fade-left">
                         <span>(2020-2021)</span>
                         <h3>front-end developer</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                             Nisi necessitatibus pariatur minima voluptatem, rem similique?</p>
                     </div>
-                    <div class="box">
+                    <div class="box" data-aos="fade-left">
                         <span>(2021-2022)</span>
                         <h3>back-end developer</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                             Nisi necessitatibus pariatur minima voluptatem, rem similique?</p>
                     </div>
-                    <div class="box">
+                    <div class="box" data-aos="fade-left">
                         <span>(2022-2023)</span>
                         <h3>Android developer</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -148,42 +184,42 @@
     <!-- service section start -->
     <section class="services" id="services">
 
-        <h1 class="heading"><span>services</span></h1>
+        <h1 class="heading" data-aos="fade-up"><span>services</span></h1>
 
         <div class="box-container">
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <i class="fa fa-code"></i>
                 <h3>web development</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur
                     adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
             </div>
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <i class="fa-brands fa-dev"></i>
                 <h3>backend development</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur
                     adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
             </div>
 
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <i class="fa-brands fa-bootstrap"></i>
                 <h3>bootstrap</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur
                     adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
             </div>
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <i class="fa fa-terminal on fa-square "></i>
                 <h3>android development</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur
                     adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
             </div>
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <i class="fa fa-bullhorn "></i>
                 <h3>digital marketing</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur
                     adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
             </div>
 
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <i class="fa-brands fa-wordpress "></i>
                 <h3>wordpress</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur
@@ -198,38 +234,38 @@
 
     <!-- portfolio section starts -->
     <section class="portfolio" id="portfolio">
-        <h1 class="heading"><span>portfolio</span></h1>
+        <h1 class="heading" data-aos="fade-up"><span>portfolio</span></h1>
 
         <div class="box-container">
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <img src="img/f-1.jpg" alt="">
                 <h3>web development</h3>
                 <span>(2020 - 2022)</span>
             </div>
 
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <img src="img/f-2.jpg" alt="">
                 <h3>web development</h3>
                 <span>(2020 - 2022)</span>
             </div>
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <img src="img/f-3.jpg" alt="">
                 <h3>web development</h3>
                 <span>(2020 - 2022)</span>
             </div>
 
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <img src="img/f-4.jpg" alt="">
                 <h3>web development</h3>
                 <span>(2020 - 2022)</span>
             </div>
 
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <img src="img/f-5.jpg" alt="">
                 <h3>web development</h3>
                 <span>(2020 - 2022)</span>
             </div>
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <img src="img/f-6.jpg" alt="">
                 <h3>web development</h3>
                 <span>(2020 - 2022)</span>
@@ -243,31 +279,31 @@
 
     <!-- contact section starts -->
     <section class="contact" id="contact">
-        <h1 class="heading"><span>contact me</span></h1>
+        <h1 class="heading" data-aos="fade-up"><span>contact me</span></h1>
 
         <form action="" method="post">
             <div class="flex">
-                <input type="text" name="name" placeholder="enter your name" class="box" required>
-                <input type="email" name="email" placeholder="enter your email" class="box" required>
+                <input type="text"  data-aos="fade-right" name="name" placeholder="enter your name" class="box" required>
+                <input type="email" data-aos="fade-left"  name="email" placeholder="enter your email" class="box" required>
             </div>
-            <input type="number" min="0" name="number" placeholder="enter your number" class="box" required>
-            <textarea name="message" class="box" placeholder="enter your message" required cols="30" rows="10"></textarea>
-            <input type="submit" value="send message" name="send" class="btn">
+            <input type="number"  data-aos="fade-up" min="0" name="number" placeholder="enter your number" class="box" required>
+            <textarea name="message" data-aos="fade-up"  class="box" placeholder="enter your message" required cols="30" rows="10"></textarea>
+            <input type="submit" data-aos="zoom-in" value="send message" name="send" class="btn">
         </form>
         <div class="box-container">
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <i class="fas fa-phone"></i>
                 <h3>phone</h3>
                 <p>+123-456-7890</p>
             </div>
 
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <i class="fas fa-envelope"></i>
                 <h3>email</h3>
                 <p>mysteriousman0947@gmail.com</p>
             </div>
 
-            <div class="box">
+            <div class="box" data-aos="zoom-in">
                 <i class="fas fa-map-marker-alt"></i>
                 <h3>address</h3>
                 <p>kolhapur ,india - 416121</p>
@@ -278,26 +314,18 @@
 
     <div class="credit"> &copy; copyright @ <?php echo date('Y'); ?> by <span>mr. rushikesh kore</span></div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     <script src="js/script.js"></script>
+
+
+   <!-- aos js link -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script> 
+ <script>
+    AOS.init({
+       duration:800,
+       delay:300
+    });
+ </script>
+
 </body>
 
 </html>
