9eb4004cd257d4d5cf669ea4188ca7e69ae8563f
Rushi047 <2020056791@dkasc.ac.in>
2024-08-31 17:59:30 +0530

    new update

--- index.php
+++ index.php
@@ -78,15 +78,188 @@
         <div class="skills">
             <h1 class="heading"><span>skills</span></h1>
             <div class="progress">
-                <div class="bar"><span>HTML</span> <span>95%</span></div>
+                <div class="bar">
+                    <h3><span>HTML</span><span>90%</span></h3>
+                </div>
+                <div class="bar">
+                    <h3><span>CSS</span><span>70%</span></h3>
+                </div>
+                <div class="bar">
+                    <h3><span>JavaScript</span><span>55%</span></h3>
+                </div>
+                <div class="bar">
+                    <h3><span>PHP</span><span>55%</span></h3>
+                </div>
+            </div>
+        </div>
+        <div class="edu-exp">
+            <h1 class="heading"><span>education & experience</span></h1>
+            <div class="row">
+                <div class="box-container">
+                    <h3 class="title">education</h3>
+                    <div class="box">
+                        <span>(2020-2021)</span>
+                        <h3>web designer</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nisi necessitatibus pariatur minima voluptatem, rem similique?</p>
+                    </div>
+                    <div class="box">
+                        <span>(2021-2022)</span>
+                        <h3>web developer</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nisi necessitatibus pariatur minima voluptatem, rem similique?</p>
+                    </div>
+                    <div class="box">
+                        <span>(2022-2023)</span>
+                        <h3>Android developer</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nisi necessitatibus pariatur minima voluptatem, rem similique?</p>
+                    </div>
+
+                </div>
+
+                <div class="box-container">
+                    <h3 class="title">experience</h3>
+                    <div class="box">
+                        <span>(2020-2021)</span>
+                        <h3>front-end developer</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nisi necessitatibus pariatur minima voluptatem, rem similique?</p>
+                    </div>
+                    <div class="box">
+                        <span>(2021-2022)</span>
+                        <h3>back-end developer</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nisi necessitatibus pariatur minima voluptatem, rem similique?</p>
+                    </div>
+                    <div class="box">
+                        <span>(2022-2023)</span>
+                        <h3>Android developer</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nisi necessitatibus pariatur minima voluptatem, rem similique?</p>
+                    </div>
+
+                </div>
             </div>
         </div>
     </section>
     <!-- about section end -->
 
+    <!-- service section start -->
+    <section class="services" id="services">
+
+        <h1 class="heading"><span>services</span></h1>
+
+        <div class="box-container">
+            <div class="box">
+                <i class="fa fa-code"></i>
+                <h3>web development</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
+            </div>
+            <div class="box">
+                <i class="fa-brands fa-dev"></i>
+                <h3>backend development</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
+            </div>
+
+            <div class="box">
+                <i class="fa-brands fa-bootstrap"></i>
+                <h3>bootstrap</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
+            </div>
+            <div class="box">
+                <i class="fa fa-terminal on fa-square "></i>
+                <h3>android development</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
+            </div>
+            <div class="box">
+                <i class="fa fa-bullhorn "></i>
+                <h3>digital marketing</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
+            </div>
+
+            <div class="box">
+                <i class="fa-brands fa-wordpress "></i>
+                <h3>wordpress</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Tenetur eveniet laudantium pariatur? Recusandae, fugiat laudantium!</p>
+            </div>
+        </div>
 
 
 
+
+    </section>
+
+
+    <!-- service section end  -->
+
+    <!-- portfolio section starts -->
+    <section class="portfolio" id="portfolio">
+        <h1 class="heading"><span>portfolio</span></h1>
+
+        <div class="box-container">
+            <div class="box">
+                <img src="img/f-1.jpg" alt="">
+                <h3>web development</h3>
+                <span>(2020 - 2022)</span>
+            </div>
+
+            <div class="box">
+                <img src="img/f-2.jpg" alt="">
+                <h3>web development</h3>
+                <span>(2020 - 2022)</span>
+            </div>
+            <div class="box">
+                <img src="img/f-3.jpg" alt="">
+                <h3>web development</h3>
+                <span>(2020 - 2022)</span>
+            </div>
+
+            <div class="box">
+                <img src="img/f-4.jpg" alt="">
+                <h3>web development</h3>
+                <span>(2020 - 2022)</span>
+            </div>
+
+            <div class="box">
+                <img src="img/f-5.jpg" alt="">
+                <h3>web development</h3>
+                <span>(2020 - 2022)</span>
+            </div>
+            <div class="box">
+                <img src="img/f-6.jpg" alt="">
+                <h3>web development</h3>
+                <span>(2020 - 2022)</span>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- portfolio section end -->
 
 
 
